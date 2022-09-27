13ad14e0b4b8fd9f6e46d40d4d41a71baf43b530
a01752785 <a01752785@tec.mx>
2022-09-27 17:53:17 -0500

    Adding the Incident Service Client class

---
+++ Application/app/src/main/java/mx/itesm/incidentesatizapan/IncidentServiceClient.kt
@@ -0,0 +1,31 @@
+package mx.itesm.incidentesatizapan
+
+import io.grpc.ManagedChannel
+import io.grpc.StatusRuntimeException
+import java.io.Closeable
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author: David Damian
+ * The IncidentServiceClient class communicates the gRPC server with the application.
+ */
+class IncidentServiceClient(private val channel: ManagedChannel) : Closeable {
+    private var stub = IncidentServiceGrpc.newBlockingStub(channel)
+
+    fun getIncidents(): Incidents {
+        val request = IncidentServiceRpcRequest.newBuilder().build()
+        try {
+            val response = stub.getIncidents(request)
+            val incidents = Incidents.newBuilder()
+            incidents.addIncident(response)
+            return incidents.build()
+        } catch (e: StatusRuntimeException) {
+            println("gRPC failed: ${e.status}")
+            return Incidents.newBuilder().build()
+        }
+    }
+
+    override fun close() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
+}
