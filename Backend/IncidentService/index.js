d0be7a4fa6e59ed23821c59d7130a7ea6f34fe24
a01752785 <a01752785@tec.mx>
2022-09-27 19:54:15 -0500

    Adding dummy data to the server and removing them from the app

--- Backend/IncidentService/index.js
+++ Backend/IncidentService/index.js
@@ -13,27 +13,68 @@ var packageDefinition = protoLoader.loadSync(
 var incidentservice = grpc.loadPackageDefinition(packageDefinition).mx.itesm.incidentesatizapan;
 
 function getIncidentsFromDb(incidentServiceRpcRequest) {
-    var incident = {
-      coordinate: {
-        latitude: 19.589693,
-        longitude: -99.229509
-      },
-      reference_location: "Calle Mariano Matamoros hola",
-      description: "Incendio de casa. Mantenerse alejados.",
-      incident_type: "WATER_LEAK",
-      risk_radius: 5
+    var incident1 = {
+        coordinate: {
+            latitude: 19.589693,
+            longitude: -99.229509
+        },
+        reference_location: "Calle Mariano Matamoros",
+        description: "Inundación por 20mm de lluvia",
+        incident_type: "FLOODING",
+        risk_radius: 500
     };
     var incident2 = {
-      coordinate: {
-        latitude: 19.589693,
-        longitude: -99.229509
-      },
-      reference_location: "Calle Mariano Matamoros",
-      description: "Incendio de casa. Mantenerse alejados.",
-      incident_type: "CAR_ACCIDENT",
-      risk_radius: 7
+        coordinate: {
+            latitude: 19.569871,
+            longitude: -99.246799
+        },
+        reference_location: "Colonia Atizapán Centro",
+        description: "Incendio de casa. Mantenerse alejados.",
+        incident_type: "FIRE",
+        risk_radius: 100
     };
-    var incidents = [incident, incident2];
+    var incident3 = {
+        coordinate: {
+            latitude: 19.589861,
+            longitude: -99.221233
+        },
+        reference_location: "Calle Azalea",
+        description: "Fuga de gas. Elementos de protección civil aproximándose.",
+        incident_type: "GAS_LEAK",
+        risk_radius: 300
+    };
+    var incident3 = {
+        coordinate: {
+            latitude: 19.570737,
+            longitude: -99.220498
+        },
+        reference_location: "Av. Paseo de los Gigantes",
+        description: "Fuga de agua. En Reparación.",
+        incident_type: "WATER_LEAK",
+        risk_radius: 50
+    };
+    var incident4 = {
+        coordinate: {
+            latitude: 19.573718,
+            longitude: -99.238168
+        },
+        reference_location: "Calle 25 de Diciembre",
+        description: "Accidente de tránsito. Ambulancias aproximándose.",
+        incident_type: "CAR_ACCIDENT",
+        risk_radius: 50
+    };
+    var incident5 = {
+        coordinate: {
+            latitude: 19.592465,
+            longitude: -99.256743
+        },
+        reference_location: "Autopista Lecheria - Chamapa",
+        description: "Árbol caído. Elementos de protección civil en el lugar.",
+        incident_type: "OTHER",
+        risk_radius: 100
+    };
+
+    var incidents = [incident1, incident2, incident3, incident4, incident5];
     console.log("call");
     return { incident : incidents };
 }
@@ -43,16 +84,16 @@ function getIncidents(call, callback) {
 }
 
 /**
- * Starts an RPC server that receives requests for the Greeter service at the
+ * Starts an RPC server that receives requests for the Incident service at the
  * sample server port
  */
 function main() {
-  var server = new grpc.Server();
-  server.addService(incidentservice.IncidentService.service, {getIncidents: getIncidents});
-  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    console.log("gRPC server started on port 50051, with services getIncidents");
-    server.start();
-  });
+    var server = new grpc.Server();
+    server.addService(incidentservice.IncidentService.service, {getIncidents: getIncidents});
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        console.log("gRPC server started on port 50051, with services getIncidents");
+        server.start();
+    });
 }
 
 main();
