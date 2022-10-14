67ae231a1a70b0ac6c068fbaaa153b6caf69a843
Adrian101-hnd <69586718+Adrian101-hnd@users.noreply.github.com>
2022-10-14 11:58:00 -0600

    Correcciones

--- Application/app/src/main/java/mx/itesm/incidentesatizapan/view/MainFragment.kt
+++ Application/app/src/main/java/mx/itesm/incidentesatizapan/view/MainFragment.kt
@@ -46,70 +46,69 @@ class MainFragment : Fragment() {
                 notifyConnectionFailure()
             }
             else{
-            var rightNow = Calendar.getInstance()
-            val today = rightNow.get(7)
-            var dia = today
-            var dias = MutableLiveData<String>()
-            binding.hoy.setText("Hoy")
-            viewModel.getdia(dia)
-            //Este for loop asigna dinamicamente los datos correctos basados en la API del Clima
-            for(i in 1..7){
-                viewModel.getIcon(it.getData(i-1).weather.icon)
-                if(i == 1){
-                    binding.htempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.htempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.htempavg.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.hoy.setText(viewModel.dia1.value.toString())
-                    binding.tvTodayMinTempVal.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.tvTodayMaxTempVal.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvCurrentTermicSensation.setText(it.getData(i-1).appMaxTemp.toString()+"°")
-                    binding.hdes.setText(it.getData(i-1).weather.description.toString())
-                    binding.imgWeekWeather1.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                    binding.imgTodayWeather.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                    binding.tvAirSpeed.setText(it.getData(i-1).windSpd.toString()+"m/s")
-                }
-                if(i == 2){
-                    binding.m1tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.m1tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvWeekDayTitle2.setText(viewModel.dia1.value.toString())
-                    binding.imgWeekWeather2.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                }
-                if(i == 3){
-                    binding.m2tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.m2tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvWeekDayTitle3.setText(viewModel.dia1.value.toString())
-                    binding.imgWeekWeather3.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                }
-                if(i == 4){
-                    binding.m3tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.m3tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvWeekDayTitle4.setText(viewModel.dia1.value.toString())
-                    binding.imgWeekWeather4.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                }
-                if(i == 5){
-                    binding.m4tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.m4tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvWeekDayTitle5.setText(viewModel.dia1.value.toString())
-                    binding.imgWeekWeather5.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                }
-                if(i == 6){
-                    binding.m5tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.m5tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvWeekDayTitle6.setText(viewModel.dia1.value.toString())
-                    binding.imgWeekWeather6.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                }
-                if(i == 7){
-                    binding.m6tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
-                    binding.m6tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
-                    binding.tvWeekDayTitle7.setText(viewModel.dia1.value.toString())
-                    binding.imgWeekWeather7.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
-                }
-                dia = dia + 1
-                if(dia > 7){
-                    dia = 1
-                }
+                var rightNow = Calendar.getInstance()
+                val today = rightNow.get(7)
+                var dia = today
+                binding.hoy.setText("Hoy")
                 viewModel.getdia(dia)
-            }
+                //Este for loop asigna dinamicamente los datos correctos basados en la API del Clima
+                for(i in 1..7){
+                    viewModel.getIcon(it.getData(i-1).weather.icon)
+                    if(i == 1){
+                        binding.htempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.htempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.htempavg.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.hoy.setText(viewModel.dia1.value.toString())
+                        binding.tvTodayMinTempVal.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.tvTodayMaxTempVal.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvCurrentTermicSensation.setText(it.getData(i-1).appMaxTemp.toString()+"°")
+                        binding.hdes.setText(it.getData(i-1).weather.description.toString())
+                        binding.imgWeekWeather1.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                        binding.imgTodayWeather.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                        binding.tvAirSpeed.setText(it.getData(i-1).windSpd.toString()+" m/s")
+                            }
+                    if(i == 2){
+                        binding.m1tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.m1tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvWeekDayTitle2.setText(viewModel.dia1.value.toString())
+                        binding.imgWeekWeather2.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                            }
+                    if(i == 3){
+                        binding.m2tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.m2tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvWeekDayTitle3.setText(viewModel.dia1.value.toString())
+                        binding.imgWeekWeather3.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                            }
+                    if(i == 4){
+                        binding.m3tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.m3tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvWeekDayTitle4.setText(viewModel.dia1.value.toString())
+                        binding.imgWeekWeather4.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                            }
+                    if(i == 5){
+                        binding.m4tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.m4tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvWeekDayTitle5.setText(viewModel.dia1.value.toString())
+                        binding.imgWeekWeather5.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                            }
+                    if(i == 6){
+                        binding.m5tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.m5tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvWeekDayTitle6.setText(viewModel.dia1.value.toString())
+                        binding.imgWeekWeather6.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                            }
+                    if(i == 7){
+                        binding.m6tempMin.setText(it.getData(i-1).minTemp.toString()+"°")
+                        binding.m6tempMax.setText(it.getData(i-1).maxTemp.toString()+"°")
+                        binding.tvWeekDayTitle7.setText(viewModel.dia1.value.toString())
+                        binding.imgWeekWeather7.setImageDrawable(getResources().getDrawable(viewModel.icon1.value!!))
+                            }
+                    dia = dia + 1
+                    if(dia > 7){
+                        dia = 1
+                        }
+                    viewModel.getdia(dia)
+                }
             }
         }
     }
